c8b5b286fecb79477087b7df9d97209de3095c35
John Siwicki <johnsiwicki@gmail.com>
2018-05-28 19:27:59 -0400

    Update quotes-api.js

--- functions/quotes-api.js
+++ functions/quotes-api.js
@@ -1,11 +1,11 @@
 exports.handler = function(event, context, callback) {
   
- const swanQuotes = [{
+ const swanQuotes = [
     "People who buy things are suckers.",
     "I'm going to get 12 eggs and part of a dead animal. Dealer's choice. Please and thank you.",
     "What the f*ck is a German muffin?!?",
     "I wanna punch you in the face so bad right now."
- }];
+ ];
 
   callback(null, {
     statusCode: 200,
