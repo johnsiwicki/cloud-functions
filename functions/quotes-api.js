09637210332e259a62e5f40597859ef61826f806
John Siwicki <johnsiwicki@gmail.com>
2018-05-28 19:26:26 -0400

    Update quotes-api.js

--- functions/quotes-api.js
+++ functions/quotes-api.js
@@ -1,17 +1,17 @@
 exports.handler = function(event, context, callback) {
   
- const quotes = [
+ const swanQuotes = {
     "People who buy things are suckers.",
     "I'm going to get 12 eggs and part of a dead animal. Dealer's choice. Please and thank you.",
     "What the f*ck is a German muffin?!?",
     "I wanna punch you in the face so bad right now."
-  ];
+ };
 
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(
       {
-         quotes
+         swanQuotes
       }
     )
   })
