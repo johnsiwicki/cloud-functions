6075db28e5e2f8d7ae50d91d5c2603f87af241d6
John Siwicki <johnsiwicki@gmail.com>
2018-05-28 19:12:31 -0400

    Update quotes-api.js

--- functions/quotes-api.js
+++ functions/quotes-api.js
@@ -1,5 +1,12 @@
-module.exports = function(context, cb) {
-  cb(null, { 
-      hello: 'Clear alcohols are for rich women on diets' 
-  });
-};
+exports.handler = function(event, context, callback) {
+  console.log(event)
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+         0:  "Clear alcohols are for rich women on diets.",
+         1: "Crying: acceptable at funerals and the Grand Canyon."
+      }
+    )
+  })
+}
