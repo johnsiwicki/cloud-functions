5e28368f0074624261164a62af0ad8847002c4da
John Siwicki <johnsiwicki@gmail.com>
2018-05-28 20:43:38 -0400

    Update quotes-api.js

--- functions/quotes-api.js
+++ functions/quotes-api.js
@@ -1,19 +1,19 @@
 exports.handler = function(event, context, callback) {
   
- const swanQuotes = [
-    "People who buy things are suckers.",
-    "I'm going to get 12 eggs and part of a dead animal. Dealer's choice. Please and thank you.",
-    "What the f*ck is a German muffin?!?",
-    "I wanna punch you in the face so bad right now."
- ];
+ const swanQuotes = {
+    0: "People who buy things are suckers.",
+    1: "I'm going to get 12 eggs and part of a dead animal. Dealer's choice. Please and thank you.",
+    2: "What the f*ck is a German muffin?!?",
+    3: "I wanna punch you in the face so bad right now."
+ };
 
   
- const goldQuotes = [
-    "It\'s a delight to trust somebody so completely.",
-    "It\'s mysterious what attracts you to a person.",
-    "I, uh, don't think I'm, y\'know, so different than your average, y\'know, average.",
-    "I love to be directed. They can trust me and go."
- ];
+ const goldQuotes = {
+   0: "It\'s a delight to trust somebody so completely.",
+   1: "It\'s mysterious what attracts you to a person.",
+   2: "I, uh, don't think I'm, y\'know, so different than your average, y\'know, average.",
+   3: "I love to be directed. They can trust me and go."
+ };
   
  callback(null, {
     statusCode: 200,
