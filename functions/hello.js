7c0a86b74161dc2f29e2d200b8463b0a518e859b
John Siwicki <johnsiwicki@gmail.com>
2018-08-12 13:19:15 -0400

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,18 @@
-exports.handler = function(event, context, callback) {
-  console.log(event)
-  callback(null, {
+'use strict';
+
+
+export function handler(event, context, callback) {
+
+  // grab the parameters from the request
+  const params = event.queryStringParameters;
+
+  // Report back to our logs
+  console.log(`You called the Lambda function with all of these parameters: ${JSON.stringify(params)}`);
+
+  // send a response
+  return callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"})
-  })
+    body: `Hello from the serverless function. Called with these parameters: ${JSON.stringify(params)}`
+  });
+
 }
