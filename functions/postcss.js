120b1c0be69711131feb36c0d414814882645c1e
John Siwicki <johnsiwicki@gmail.com>
2018-10-05 10:31:43 -0400

    Update postcss.js

--- functions/postcss.js
+++ functions/postcss.js
@@ -1,1 +1,20 @@
 
+const postcss = require('postcss');
+const cssvariables = require('postcss-css-variables');
+const pFor = require('postcss-for');
+const clean = require('postcss-clean');
+
+module.exports = function(context, cb) {
+  const mycss = context.body.pCSS;
+  
+  var output = postcss([
+		cssvariables(/*options*/),
+		pFor(),
+		clean()
+	])
+	.process(mycss)
+	.css;
+	
+  cb(null, {output});
+
+};
