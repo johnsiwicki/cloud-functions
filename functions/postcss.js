d80a0dd15f666694de7f4b0cdbfce57ddb2e6b98
John Siwicki <johnsiwicki@gmail.com>
2018-10-05 11:24:25 -0400

    Update postcss.js

--- functions/postcss.js
+++ functions/postcss.js
@@ -1,8 +1,8 @@
 
-import postcss from 'postcss';
-import cssvariables from 'postcss-css-variables';
-import pFor from 'postcss-for';
-import clean from 'postcss-clean';
+const postcss = require('postcss');
+const cssvariables = require('postcss-css-variables');
+const pFor = require('postcss-for');
+const clean = require('postcss-clean');
 
 
 exports.handler = function(event, context, callback) {
