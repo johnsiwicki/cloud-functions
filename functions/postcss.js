42fdf851b4bd7a4b66865919cdd10f660f127660
John Siwicki <johnsiwicki@gmail.com>
2018-10-05 11:13:03 -0400

    Update postcss.js

--- functions/postcss.js
+++ functions/postcss.js
@@ -1,10 +1,13 @@
 
+
+exports.handler = function(event, context, callback) {
+
+	
 const postcss = require('postcss');
 const cssvariables = require('postcss-css-variables');
 const pFor = require('postcss-for');
 const clean = require('postcss-clean');
-
-exports.handler = function(event, context, callback) {
+	
   const mycss = context.body.pCSS;
   
   var output = postcss([
