d04e0a67451e05a11eb646ec87a87b8707406dc5
John Siwicki <johnsiwicki@gmail.com>
2019-03-25 21:18:52 -0400

    Update rss-json.js

--- functions/rss-json.js
+++ functions/rss-json.js
@@ -1,14 +1,15 @@
-import fetch from "node-fetch";
-import xml2js from "xml2js-es6-promise";
+import Feed = from "rss-to-json";
 
 
 exports.handler = async (event, context, callback) => {
-    const mediumFeed = `https://codepen.io/posts/feed`
-    const response = await fetch(mediumFeed)
-    const xml = await response.text()
-    const data = await xml2js(xml)
+    var jsonfeed;
+    Feed.load('https://codek.tv/feed/', function(err, rss){
+        var jsonfeed = rss;
+     });
+  
     return  {
       statusCode: 200,
-      body: JSON.stringify(data.rss.channel[0].item)
+      body: jsonfeed
     }
 }
+
