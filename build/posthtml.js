b329a06a43fbe6e0b81d6fd770fec97ed1aa20f5
John Siwicki <johnsiwicki@gmail.com>
2019-03-26 08:42:46 -0400

    Update build/posthtml.js

--- build/posthtml.js
+++ build/posthtml.js
@@ -4,8 +4,7 @@ import posthtmlalt from "posthtml-alt-always";
 import posthtmlinc from "posthtml-include";
 
 exports.handler = function (event, context, callback) {
-    console.log(context.body.email);
-  const bEmail = context.body.email;
+  const bEmail = event.body.email;
   const result =  
         posthtml([
       		posthtmlce(),
