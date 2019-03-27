82ae74c14554f05e86b09741a8ecb5e9338b4a82
John Siwicki <johnsiwicki@gmail.com>
2019-03-27 17:06:21 -0400

    stuff

--- build/render-pug.js
+++ build/render-pug.js
@@ -1,10 +1,11 @@
-
 import pug from "pug";
+
 exports.handler = function(event, context, callback) {
   //store our pug
   const puggy = event.body;
   //compile our pug
   const compiledFunction = pug.render(puggy);
+  console.log(compiledFunction);
   	callback(null, {
   	  statusCode: 200,
   	  //body: JSON.stringify(output);
