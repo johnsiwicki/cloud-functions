c10947177c8774336b7bd9037ea73d3966882306
John Siwicki <johnsiwicki@gmail.com>
2019-03-27 16:10:05 -0400

    something happened

--- build/postcss.js
+++ build/postcss.js
@@ -1,26 +1,18 @@
 import postcss from "postcss";
 import cssvariables from "postcss-css-variables";
-import pFor from "postcss-for";
-import clean from "postcss-clean";
+ 
 
 exports.handler = function(event, context, callback) {
-
-			const mycss = event.body;
-			
+		  const mycss = event.body;		
 			var output = postcss([
-				cssvariables(/*options*/),
-				pFor(),
-				clean()
+				cssvariables(),
 			])
 			.process(mycss)
 			.css;
-
-			   console.log(output);
-	
-  callback(null, {
-  statusCode: 200,
-  	body: JSON.stringify(output)
-  });
+		callback(null, {
+			statusCode: 200,
+				body: JSON.stringify(output)
+		});
 };
 
  
