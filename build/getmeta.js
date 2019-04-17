2ccda85b4367894bdda21c304f00fd1938ad033c
John Siwicki <johnsiwicki@gmail.com>
2019-04-17 08:36:49 -0400

    new build

--- build/getmeta.js
+++ build/getmeta.js
@@ -4,10 +4,12 @@ import urlMetadata from "url-metadata";
 exports.handler = function (event, context, callback) {
 
     urlMetadata(event.queryStringParameters.url).then(
-            callback(null, {
+            function (metadata) { // success handler
+               callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(metadata)
-            }),
+                });
+            },
         function (error) { // failure handler
             console.log(error);
         }
