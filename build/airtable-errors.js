e733e0350d69f4b6c64c43550c1e47194d8db2b5
John Siwicki <johnsiwicki@gmail.com>
2019-04-21 15:13:27 -0400

    header tokens

--- build/airtable-errors.js
+++ build/airtable-errors.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch"
+const AIRTABLE_API_KEY = process.env;
 
 exports.handler = function(event, context, callback) {
     var url = 'https://api.airtable.com/v0/app942GISbGAKfB4f/Errors';
@@ -21,12 +22,12 @@ exports.handler = function(event, context, callback) {
         mode: "no-cors",
 		body: JSON.stringify(data), // data can be `string` or {object}!
 		headers:{
-			'Content-Type': 'application/json'
-		}
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' `${AIRTABLE_API_KEY}`
+		} 
 	}).then(res => res.json())
 	.then(response => console.log('Success:', JSON.stringify(response)))
     .catch(error => console.error('Error:', error));
     
- 
 
 }
