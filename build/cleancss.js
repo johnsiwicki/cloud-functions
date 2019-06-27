da14887cbd879a9c338e19d565c2993c547d81bd
John Siwicki <johnsiwicki@gmail.com>
2019-06-26 22:18:05 -0400

    Update cleancss.js

--- build/cleancss.js
+++ build/cleancss.js
@@ -1,13 +1,18 @@
- var CleanCSS = require('clean-css');
+var CleanCSS = require('clean-css');
 
 exports.handler = function(event, context, callback) {
- 
-  var input = 'a{font-weight:bold;}';
-  var options = { /* options */ };
-  var output =  new CleanCSS().minify(['https://a80427d48f9b9f165d8d-c913073b3759fb31d6b728a919676eab.ssl.cf1.rackcdn.com/v3/colorbox/colorbox.css', 'https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css']);
- 
-              callback(null, {
+     var options = { /* options */ };
+     var source = '@import url(https://a80427d48f9b9f165d8d-c913073b3759fb31d6b728a919676eab.ssl.cf1.rackcdn.com/v3/colorbox/colorbox.css);';
+    new CleanCSS({ inline: ['remote'] }).minify(source, function (error, output) {
+    // output.styles
+     callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(output)
                 });
+     
+ });
+ 
+              
 }
+
+
