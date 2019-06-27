9b21b6a990bb2ee276318ac3777f874b47284c7c
John Siwicki <johnsiwicki@gmail.com>
2019-06-26 22:06:10 -0400

    Update cleancss.js

--- build/cleancss.js
+++ build/cleancss.js
@@ -1,4 +1,7 @@
-var CleanCSS = require('clean-css');
-var input = 'a{font-weight:bold;}';
-var options = { /* options */ };
-var output = new CleanCSS(options).minify(input);
+ var CleanCSS = require('clean-css');
+
+exports.handler = function(event, context, callback) {
+  var input = 'a{font-weight:bold;}';
+  var options = { /* options */ };
+  var output = new CleanCSS(options).minify(input);
+}
